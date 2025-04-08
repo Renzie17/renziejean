f5592b3b12cc476e319409067254afa055d58540
Renzie17 <146044463+Renzie17@users.noreply.github.com>
2025-04-08 16:22:38 +0800

    activity 4

---
+++ about.php
@@ -0,0 +1,20 @@
+ <!-- About section -->
+ <section id="about">
+    <div class="container">
+      <div class="row align-items-center">
+        
+        <div class="col-lg-6 pe-5">
+          <h3>Lorem, ipsum</h3>
+          <h2>lorem ipsum dolor sit amet.</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quasi dolor natus, autem exercitationem
+            aut provident facere optio impedit perspiciatis dolorem, expedita porro dignissimos, mollitia magni! Fugiat
+            eius odit hic.
+          </p>
+            <a href="g" class="btn btn-renzie">buttom</a>
+          </div>
+            <div class="col-lg-6">
+              <img src="vector (1).jpg" alt="">
+           </div>   
+      </div>
+    </div>
+    </section>
---
+++ services.php
@@ -0,0 +1,63 @@
+<!-- Services -->
+<section id="services">
+      <div class="container">
+        <h2 class="text-center">SERVICES</h2>
+        <div class="row row-cols-lg-3">
+          <div class="col d-flex jusify-content-center">
+            <div class="card" style="width: 24rem;">
+              <img src="flower-bouquet.png" class="card-img-top mx-auto" alt="...">
+              <div class="card-body text-center">
+                <h5 class="card-title">Card Title</h5>
+                <p class="card-text">Some quick example to build on the card title and make up the bulk of the cards content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col d-flex jusify-content-center">
+            <div class="card" style="width: 24rem;">
+              <img src="flower-bouquet.png" class="card-img-top mx-auto" alt="...">
+              <div class="card-body text-center">
+                <h5 class="card-title">Card Title</h5>
+                <p class="card-text">Some quick example to build on the card title and make up the bulk of the cards content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col d-flex jusify-content-center">
+            <div class="card" style="width: 24rem;">
+              <img src="flower-bouquet.png" class="card-img-top mx-auto" alt="...">
+              <div class="card-body text-center">
+                <h5 class="card-title">Card Title</h5>
+                <p class="card-text">Some quick example to build on the card title and make up the bulk of the cards content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col d-flex jusify-content-center">
+            <div class="card" style="width: 24rem;">
+              <img src="flower-bouquet.png" class="card-img-top mx-auto" alt="...">
+              <div class="card-body text-center">
+                <h5 class="card-title">Card Title</h5>
+                <p class="card-text">Some quick example to build on the card title and make up the bulk of the cards content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col d-flex jusify-content-center">
+            <div class="card" style="width: 24rem;">
+              <img src="flower-bouquet.png" class="card-img-top mx-auto" alt="...">
+              <div class="card-body text-center">
+                <h5 class="card-title">Card Title</h5>
+                <p class="card-text">Some quick example to build on the card title and make up the bulk of the cards content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col d-flex jusify-content-center">
+            <div class="card" style="width: 24rem;">
+              <img src="flower-bouquet.png" class="card-img-top mx-auto" alt="...">
+              <div class="card-body text-center">
+                <h5 class="card-title">Card Title</h5>
+                <p class="card-text">Some quick example to build on the card title and make up the bulk of the cards content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+  </section>
